0e12ffaf8241b0826d901f56228f0860684f12d3
ljy <1315104341@qq.com>
2025-09-15 17:26:04 +0800

    update:多模态key_shape更新

--- YOLOFuse-main/train_dual.py
+++ YOLOFuse-main/train_dual.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("ultralytics/cfg/models/fuse/Early-level-Feature-Fusion-pose.yaml")
+    model = YOLO("ultralytics/cfg/models/v11/yolo11-Early-level-Feature-Fusion-pose.yaml")
     model.train(
-        data="ultralytics/cfg/datasets/coco8-pose.yaml",
+        data="ultralytics/cfg/datasets/lll-pose.yaml",
         ch=6, # 多模态时设置为 6 ，单模态时设置为 3
         imgsz=640,
         epochs=80,
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         patience=0,
         amp=False,
         cache=True, # disk 硬盘，速度稍快精度可复现；ram/True 内存，速度快但精度不可复现
-        project="runs/xxxxxxxxxxxxxxxx1",
-        name="Early-level-Feature-Fusion",
+        project="runs/pose/yolo11-Early-level-Feature-Fusion-pose.yaml",
+        name="80e-32b-again",
         resume=False,
         fraction=1, # 只用全部数据的 ？% 进行训练 (0.1-1)
     )
